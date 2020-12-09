a07c395d1ef8814c652c58556e697c3c1cde1432
Matheus Franco <matheusefranco@gmail.com>
2020-12-09 10:40:57 -0300

    Aula1

---
+++ CadastroBandas/ConectaBanco.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using System.Data;
+namespace CadastroBandas
+{
+    class ConectaBanco
+    {
+        MySqlConnection conexao = new MySqlConnection("server=localhost;user id=root;password=compServer;database=banco_bandas");
+        public string mensagem;
+
+        public DataTable lista()
+        {
+            MySqlCommand cmd = new MySqlCommand("listaBandas", conexao);
+            cmd.CommandType = CommandType.StoredProcedure;
+            try
+            {
+                conexao.Open();// conectando com o banco
+                MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+                DataTable dados = new DataTable();
+                da.Fill(dados);
+                return dados;
+            }
+            catch(MySqlException erro)
+            {
+                mensagem = "Erro MySQL:" + erro.Message;
+                return null;
+            }
+            finally
+            {
+                conexao.Close();
+            }
+
+        }// fim lista
+    }
+}
---
+++ CadastroBandas/Sistema.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace CadastroBandas
+{
+    public partial class Sistema : Form
+    {
+        
+
+        public Sistema()
+        {
+            InitializeComponent();
+            
+        }
+        
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnCadastra_Click(object sender, EventArgs e)
+        {
+            marcador.Height = btnCadastra.Height;
+            marcador.Top = btnCadastra.Top;
+            tabControl1.SelectedTab = tabControl1.TabPages[0];
+            
+
+        }
+
+        
+
+        private void btnBusca_Click(object sender, EventArgs e)
+        {
+            marcador.Height = btnBusca.Height;
+            marcador.Top = btnBusca.Top;
+            tabControl1.SelectedTab = tabControl1.TabPages[1];
+        }
+
+        
+
+   
+
+
+        private void btnConfirmaRemocao_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void BtnConfirmaCadastro_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        void limpaForm()
+        {
+           
+        }
+
+        void listaBandas()
+        {
+            ConectaBanco con = new ConectaBanco();
+            dgBandas.DataSource = con.lista();
+            dgBandas.Columns["idbandas"].Visible = false;
+        }
+
+
+        private void Sistema_Load(object sender, EventArgs e)
+        {
+            listaBandas();
+        }
+
+        private void txtnomeBusca_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAlterar_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
